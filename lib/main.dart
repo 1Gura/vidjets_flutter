da25846bd9098dfda10278f91949f0c7633b03f4
Gura Ilya <igura@t1-consulting.ru>
2023-03-26 15:40:35 +0300

    Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var loading = false;
+  var progressValue = 0.0;
+
+  @override
+  void initState() {
+    loading = false;
+    progressValue = 0.0;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,27 +35,47 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
             child: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              LinearProgressIndicator(value: 23),
-              Text(
-                "23 %",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-              Text(
-                "Press button to download",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              )
-            ],
-          ),
-        )),
+                padding: EdgeInsets.all(16),
+                child: loading
+                    ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    LinearProgressIndicator(value: progressValue),
+                    Text(
+                      "${(progressValue * 100).round()}%",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    )
+                  ],
+                )
+                    : Text(
+                  "Press button to download",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ))),
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () {
+            setState(() {
+              loading = !loading;
+              _updateProgress();
+            });
+          },
           child: Icon(Icons.cloud_download),
         ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    const onSec = const Duration(seconds: 1);
+    Timer.periodic(onSec, (Timer t) {
+      setState(() {
+        progressValue += 0.2;
+        if (progressValue.toStringAsFixed(1) == "1.0") {
+          loading = false;
+          t.cancel();
+          progressValue = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
